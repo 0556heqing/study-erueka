69816c356c7db9423e4f4ad227f74d41a30773fd
heqing <heqing@yiyaowang.com>
2021-11-11 16:45:26 +0800

    erueka总结

---
+++ eureka-client/src/main/java/com/heqing/learn/EurekaClientApplication.java
@@ -0,0 +1,19 @@
+package com.heqing.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author heqing
+ * @date 2021/11/8 18:31
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+}
